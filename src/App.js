b798c8ee2c31e05ea64a35f818f1085fa28468d4
gioin <injgia15@gmail.com>
2023-03-02 13:12:36 +0400

    valid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
-  const [prodactInputValue, setProdactInputValue] = useState("");
+  const [prodactInputValue, setProdactInputValue] = useState('');
   const [validValues, setValidValues] = useState(true);
-  const [quantityInputValue, setQuantityInputValue] = useState("");
+  const [quantityInputValue, setQuantityInputValue] = useState('');
   // const [validQuantity, setValidQuantity] = useState('');
-  const [productionInputValue, setProductionInputValue] = useState("");
+  const [productionInputValue, setProductionInputValue] = useState('');
   // const [validProduction, setValidProduction] = useState('');
 
   // const [btnValue, setBtnValue] = useState(false);
@@ -21,48 +21,58 @@ function App() {
   // console.log(prodactInputValue);
   // console.log(quantityInputValue);
   // console.log(productionInputValue);
-  const ItemsCon = document.querySelector(".itemsConteiner");
-  const itemsC = document.querySelector(".itemsC");
+  const ItemsCon = document.querySelector('.itemsConteiner');
+  const itemsC = document.querySelector('.itemsC');
+
+  const label1 = document.querySelector('.label1');
+  const label2 = document.querySelector('.label2');
+  const label3 = document.querySelector('.label3');
 
   const handleButton = () => {
     // console.log('clicked')
 
     if (
-      prodactInputValue.length === 0 ||
-      quantityInputValue.length === 0 ||
-      productionInputValue.length === 0
+      (validValues && prodactInputValue.length <= 0) ||
+      (validValues && quantityInputValue.length <= 0) ||
+      (validValues && productionInputValue.length <= 0)
     ) {
+      label1.innerText = 'ველი უნდა იყოს შევსებული';
+      label2.innerText = 'ველი უნდა იყოს შევსებული';
+      label3.innerText = 'ველი უნდა იყოს შევსებული';
     } else {
-      ItemsCon.style.display = "block";
+      label1.innerText = '';
+      label2.innerText = '';
+      label3.innerText = '';
+
+      ItemsCon.style.display = 'block';
 
       const newItems = {
         id: Math.random() * 100,
         prodact: prodactInputValue,
         quantity: quantityInputValue,
-        production: productionInputValue
+        production: productionInputValue,
       };
 
-      setTodos((oldItems) => [...oldItems, newItems]);
+      setTodos(oldItems => [...oldItems, newItems]);
 
-      setProdactInputValue("");
-      setQuantityInputValue("");
-      setProductionInputValue("");
+      setProdactInputValue('');
+      setQuantityInputValue('');
+      setProductionInputValue('');
     }
   };
 
-  const handlebtnPlus = (todo) => {
+  const handlebtnPlus = todo => {
     // console.log(id);
     setBoxValue(boxValue + 1);
     // todos.map(todo => { return (todo.id === id ? (todo.quantity -= 1) : null) })
 
     //ამას ვერ გავუგე ....
     if (todo.quantity > 0) {
-
-      const updatedTodo = todos.map((t) => {
+      const updatedTodo = todos.map(t => {
         if (t.id === todo.id) {
           return {
             ...t,
-            quantity: t.quantity - 1
+            quantity: t.quantity - 1,
           };
         }
         return t;
@@ -70,9 +80,7 @@ function App() {
 
       setTodos(updatedTodo);
     } else {
-      let filteredData = todos.filter(
-        (t) => t.id !== todo.id || t.quantity > 1
-      );
+      let filteredData = todos.filter(t => t.id !== todo.id || t.quantity > 1);
       setTodos(filteredData);
     }
 
@@ -89,75 +97,63 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div className="headerConteiner">
-        <div className="inputConteiner">
-          <label htmlFor="prodact"> Prodact </label>
+    <div className='App'>
+      <div className='headerConteiner'>
+        <div className='inputConteiner'>
+          <label htmlFor='prodact'> Prodact </label>
           <input
-            onChange={(e) => setProdactInputValue(e.target.value)}
+            onChange={e => setProdactInputValue(e.target.value)}
             value={prodactInputValue}
-            type="text"
-            id="prodact"
+            type='text'
+            id='prodact'
           />
-          <div className="valid">
-            {validValues && prodactInputValue <= 0 ? (
-              <label> ველი უნდა იყოს შევსებული </label>
-            ) : (
-              ""
-            )}
+          <div className='valid'>
+            <label className='label1'> </label>
           </div>
 
-          <label htmlFor="quantity"> Quantitiy </label>
+          <label htmlFor='quantity'> Quantitiy </label>
           <input
-            onChange={(e) => setQuantityInputValue(e.target.value)}
+            onChange={e => setQuantityInputValue(e.target.value)}
             value={quantityInputValue}
-            type="text"
-            id="quantity"
+            type='text'
+            id='quantity'
           />
 
-          <div className="valid">
-            {validValues && quantityInputValue <= 0 ? (
-              <label> ველი უნდა იყოს შევსებული </label>
-            ) : (
-              ""
-            )}
+          <div className='valid'>
+            <label className='label2'> </label>
           </div>
 
-          <label htmlFor="production"> Production Country </label>
+          <label htmlFor='production'> Production Country </label>
           <input
-            onChange={(e) => setProductionInputValue(e.target.value)}
+            onChange={e => setProductionInputValue(e.target.value)}
             value={productionInputValue}
-            type="text"
-            id="production"
+            type='text'
+            id='production'
           />
 
-          <div className="valid">
-            {validValues && productionInputValue <= 0 ? (
-              <label> ველი უნდა იყოს შევსებული </label>
-            ) : (
-              ""
-            )}
+          <div className='valid'>
+            <label className='label3'> </label>
           </div>
 
-          <button onClick={handleButton} className="button">
+          <button onClick={handleButton} className='button'>
             ADD
           </button>
         </div>
 
-        <div className="BoxConteiner">{boxValue}</div>
+        <div className='BoxConteiner'>{boxValue}</div>
       </div>
 
-      <div className="itemsConteiner">
-        {todos.map((todo) => {
+      <div className='itemsConteiner'>
+        {todos.map(todo => {
           return (
-            <div className="itemsC">
+            <div className='itemsC'>
               <ul>
                 <li> Prodact: {todo.prodact}</li>
                 <li> Quantity: {todo.quantity}</li>
                 <li> Production Country: {todo.production}</li>
               </ul>
 
-              <button onClick={() => handlebtnPlus(todo)} className="btn2">
+              <button onClick={() => handlebtnPlus(todo)} className='btn2'>
                 დაამატე კალათაში
               </button>
             </div>
